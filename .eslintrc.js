88a8d0cd33afb858c99901d70acdbb8587a920d5
giogocadze <giogocadze5@gmail.com>
2025-07-01 20:48:48 +0400

    feat: added husky, commitlint, prettier, eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'next/core-web-vitals',
+      'prettier',
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'react/react-in-jsx-scope': 'off', // not needed in Next.js
+      'no-console': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  };
+  
