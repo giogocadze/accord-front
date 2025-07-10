1ca4ffb76aca7797bde7165e9f55c50ea57f84c6
Nick <jurkhaasanidzeback12@gmail.com>
2025-07-11 02:00:28 +0400

    fix: resolve header component structure and styling

--- app/shared/components/Header/Header.tsx
+++ app/shared/components/Header/Header.tsx
@@ -1,22 +1,16 @@
 'use client';
 
 import Image from 'next/image';
-import Link from 'next/link';
 import React, { useState } from 'react';
 import Input from '../Input/Input';
 import styles from './Header.module.scss';
 import BurgerIcon from './components/BurgerIcon/BurgerIcon';
 import { BurgerIconModeEnum } from './components/BurgerIcon/enums/burger-icon.enum';
 import LogoutButton from './components/LogoutButton/LogoutButton';
+import NavLinks from './components/NavLink/NavLinks';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [activeLink, setActiveLink] = useState('Home');
-  const navLinks = ['Home', 'Playlist', 'Song', 'Album', 'Artist', 'Chart'];
-
-  const handleLinkClick = (link: string) => {
-    setActiveLink(link);
-  };
 
   return (
     <header className={styles.background}>
@@ -27,16 +21,7 @@ const Header = () => {
               <Image src={'./Logo/Music, Audio/Note.svg'} width={56} height={56} alt="Logo" />
             </div>
             <nav className={`${styles.nav} ${isOpen ? styles.navOpen : ''}`}>
-              {navLinks.map(link => (
-                <Link
-                  key={link}
-                  href="#"
-                  className={link === activeLink ? styles.active : styles.link}
-                  onClick={() => handleLinkClick(link)}
-                >
-                  {link}
-                </Link>
-              ))}
+              <NavLinks />
               {isOpen && (
                 <div className={styles.logoutMobile}>
                   <LogoutButton />
