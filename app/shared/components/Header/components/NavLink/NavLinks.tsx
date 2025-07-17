1af17ea846cd7f36dff7e2bdda50d0ac654a7262
Nick <jurkhaasanidzeback12@gmail.com>
2025-07-17 17:02:26 +0400

    fix: navlinks

--- app/shared/components/Header/components/NavLink/NavLinks.tsx
+++ app/shared/components/Header/components/NavLink/NavLinks.tsx
@@ -3,10 +3,10 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import styles from './NavLinks.module.scss';
+import { navLinks } from './constants/navigation.constants';
 
 const NavLinks = () => {
   const [activeLink, setActiveLink] = useState('Home');
-  const navLinks = ['Home', 'Playlist', 'Song', 'Album', 'Artist', 'Chart'];
 
   const handleLinkClick = (link: string) => {
     setActiveLink(link);
