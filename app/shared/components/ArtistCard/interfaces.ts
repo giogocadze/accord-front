cfe39a9650ce12d877da7b0649714e77325f9146
dimitrimarshava21 <dima.marshava.@gmail.com>
2025-07-21 18:29:44 +0400

    feat: created a top artist

--- app/shared/components/ArtistCard/interfaces.ts
+++ app/shared/components/ArtistCard/interfaces.ts
@@ -1,14 +1,8 @@
 export interface Artist {
-  imageSrc: string;
   name: string;
+  imageSrc: string;
 }
 
 export interface ArtistCardProps {
-  artist1?: Artist;
-  artist2?: Artist;
-  artist3?: Artist;
-  artist4?: Artist;
-  artist5?: Artist;
-  artist6?: Artist;
   artists: Artist[];
 }
