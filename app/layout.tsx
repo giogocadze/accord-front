e36856d280abb02b2361274d8cec4f9cbbe5ff1a
giogocadze <giogocadze5@gmail.com>
2025-07-24 01:26:17 +0400

    refactor: updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
         <Header />
+        {children}
       </body>
     </html>
   );
